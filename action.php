c8918a546db3212653605ee48f43622d9221ce8e
yashbavishi <ybavishi@bu.edu>
2017-03-21 20:16:22 -0400

    Add files via upload

--- action.php
+++ action.php
@@ -2,10 +2,13 @@
 <?php
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-
+$region = $_GET['region'];
+echo($region);
 $conn = new mysqli("localhost", "root", "", "Brazil1");
-
-$result = $conn->query("SELECT * FROM y2000");
+$request1 = "SELECT * FROM y2000 WHERE reigon = ";
+$request1 .= $region;
+echo($request1);
+$result = $conn->query($request1);
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
